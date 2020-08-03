95fa8eab4aa48396a5971c08b34c3aece0e780b7
sanykos <kosih2009@yandex.ru>
2020-08-03 20:25:40 +0300

    get token

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -17,7 +17,7 @@ class Login extends Component {
                 touched: false,
                 validation: {
                     required: true,
-                    minLength: 10
+                    minLength: 1
                 }
             },
             password: {
@@ -30,26 +30,26 @@ class Login extends Component {
                 touched: false,
                 validation: {
                     required: true,
-                    minLength: 10
+                    minLength: 1
                 }
             }
         }
     }
 
-    loginHandler = () => {
+    // loginHandler = () => {
 
-    }
+    // }
 
     handleSubmit = async (e) => {
         e.preventDefault()
-        const {username, password} = this.state
+       // const {username, password} = this.state
        try {
         const response = await axios.post('/api-token-auth/',
         {
-            username: username,
-            password: password
+            username: this.state.formControls.username.value,
+            password: this.state.formControls.password.value
         })
-        //console.log(response + 'dsadsad')
+        console.log(response.data.token)
        }catch(error) {
            if(error) {
             this.setState({errorMessage: 'Неверный логин или пароль'})
@@ -92,15 +92,6 @@ class Login extends Component {
         })
     }
 
-    // handleChange = (e) => {
-    //     const value = e.currentTarget.value
-    //     const fieldName = e.currentTarget.dataset.fieldName
-    //     this.setState(prev => ({
-    //     ...prev,
-    //     [fieldName]: value,
-    //     }))
-    // }
-
     renderInputs() {
         const inputs = Object.keys(this.state.formControls).map((controlName, index) => {
             //console.log(controlName)
@@ -133,30 +124,6 @@ class Login extends Component {
                 {errorMessage && <strong>{errorMessage}</strong>}
                 <form onSubmit={this.handleSubmit}>
                     {this.renderInputs()}
-                    {/* <Input 
-                        label="username"
-                        placeholder="Enter username"
-                    />
-                    <Input 
-                        label="password"
-                        placeholder="Enter password"
-                        errorMessage="ASDASDA"
-                    /> */}
-                {/* <div className="form-group">
-                    <label htmlFor="exampleInputEmail1">Email address</label>
-                    <input type="text" className="form-control" name="username" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter username"
-                    data-field-name={'username'}
-                    onChange={this.handleChange}
-                    value={username}/>
-                    <small className="form-text text-muted">We'll never share your email with anyone else.</small>
-                </div> */}
-                {/* <div className="form-group">
-                    <label htmlFor="exampleInputPassword1">Password</label>
-                    <input type="text" name="password" className="form-control"  placeholder="Password"
-                    onChange={this.handleChange}
-                    data-field-name={'password'}
-                    value={password}/>
-                </div> */}
                 <button type="submit" className="btn btn-primary" disabled={!this.state.isFormValid}>Войти</button>
             </form>
             </div>
