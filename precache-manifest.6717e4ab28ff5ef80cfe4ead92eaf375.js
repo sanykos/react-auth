547fe7e7a892fe9b94a38fa3cae62aa8aaf10966
sanykos <kosih2009@yandex.ru>
2020-08-04 21:52:51 +0300

    Updates

---
+++ precache-manifest.6717e4ab28ff5ef80cfe4ead92eaf375.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff4e1bc04057b7f4772c8e40f8d5f87e",
+    "url": "/react-auth/index.html"
+  },
+  {
+    "revision": "dcac77aad077f0e0750b",
+    "url": "/react-auth/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "9e42a01bd95e48522149",
+    "url": "/react-auth/static/css/main.91622052.chunk.css"
+  },
+  {
+    "revision": "dcac77aad077f0e0750b",
+    "url": "/react-auth/static/js/2.2212b293.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/react-auth/static/js/2.2212b293.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e42a01bd95e48522149",
+    "url": "/react-auth/static/js/main.68d32c3c.chunk.js"
+  },
+  {
+    "revision": "ccc43d22ce1f5497b0c8",
+    "url": "/react-auth/static/js/runtime-main.3b3446a8.js"
+  }
+]);
