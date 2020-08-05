506355acf94eb531ab24ea05ac57eb1096d3341c
sanykos <kosih2009@yandex.ru>
2020-08-05 18:17:59 +0300

    change url

--- src/axios/axios-swagger.js
+++ src/axios/axios-swagger.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
 export default axios.create({
-    baseURL: 'http://emphasoft-test-assignment.herokuapp.com'
+    baseURL: 'https://emphasoft-test-assignment.herokuapp.com'
 })
